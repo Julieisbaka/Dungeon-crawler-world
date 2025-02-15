b89ffa86032e2622a7e9cdfec963ec9b08f7bc2f
Julieisbaka <casperschorr06@gmail.com>
2025-02-15 18:59:18 -0500

    Remove greetings workflow, update package version and dependencies, and add Sound class implementation

---
+++ include/audio/Sound.cpp
@@ -0,0 +1,31 @@
+#include "Sound.hpp"
+#include <string>
+
+Sound::Sound()
+{
+}
+
+bool Sound::load(const std::string &path)
+{
+  return false;
+}
+
+void Sound::play()
+{
+}
+
+void Sound::stop()
+{
+}
+
+void Sound::setVolume(float volume)
+{
+}
+
+void Sound::setLooping(bool loop)
+{
+}
+
+Sound::~Sound()
+{
+}
