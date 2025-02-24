4498cac5acbc303cee1994d9a77f187f6e271680
Julie - Mathman <casperschorr06@gmail.com>
2025-02-23 20:29:00 -0500

    feat: update project version to 0.0.4.0 and improve random time generation function

--- Floor/Floor 1/Time.cpp
+++ Floor/Floor 1/Time.cpp
@@ -5,7 +5,7 @@ int generateRandomTime() {
   std::mt19937 gen(rd());
   std::normal_distribution<> d(15, 2); // Mean 15, standard deviation 2
 
-  int randomTime = std::round(d(gen));
+  int randomTime = std::lround(d(gen));
   if (randomTime < 12)
     randomTime = 12;
   if (randomTime > 20)
