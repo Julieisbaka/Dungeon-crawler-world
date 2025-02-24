12fb2253f6f36b5ae498715340b39a41f96590f7
Julie - Mathman <casperschorr06@gmail.com>
2025-02-23 19:02:16 -0500

    feat: add random time generation function with constraints

---
+++ Floor/Floor 1/Time.cpp
@@ -0,0 +1,15 @@
+#include <random>
+
+int generateRandomTime() {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::normal_distribution<> d(15, 2); // Mean 15, standard deviation 2
+
+  int randomTime = std::round(d(gen));
+  if (randomTime < 12)
+    randomTime = 12;
+  if (randomTime > 20)
+    randomTime = 20;
+
+  return randomTime;
+}
