cc1ee5ce08c6b343b00ca380e169bd7ee1f8eabd
Julie - Mathman <casperschorr06@gmail.com>
2025-03-28 19:16:19 -0400

    Fix typos in Protective_shell spell info and add duration and radius calculations

--- Magic/Spells/Protective_shell/Bonus.cs
+++ Magic/Spells/Protective_shell/Bonus.cs
@@ -1,1 +1,3 @@
-// This program calculates the stats of the thing when used additonally to the json.
+// This program calculates the stats of the thing when used additionally to the json.
+int duration = 5 + level;
+int radius = 300 + 30 * level;
