7e4f75f90d70c1a1920526a9bf1e704d64416a96
Julie - Mathman <casperschorr06@gmail.com>
2025-04-18 16:31:23 -0400

    Delete Magic/Spells/Protective_shell/Bonus.cs

--- Magic/Spells/Protective_shell/Bonus.cs
+++
@@ -1,4 +0,0 @@
-//TODO: #11 Add multiplicative and addictive duration modifiers, add multiplicative and addictive range modifiers.
-
-int duration = (5 + level); // level is spell level not player level
-int radius = (300 + (30 * level));
