80635fbd954ee2aff527e0b517fab8e3f2b2b2d7
Julie - Mathman <casperschorr06@gmail.com>
2025-05-03 16:25:14 -0400

    fix: correct comment formatting and ensure variable declarations are properly defined

--- Magic/Spells/Protective_shell/Bonus.cs
+++ Magic/Spells/Protective_shell/Bonus.cs
@@ -1,9 +1,9 @@
 // level is spell level not player level
-//IMPORTANT: Var values for modifiers are temporary until their code is added.
-var level = 1;
-var mult_duration = 1;
-var mult_range = 1;
-var add_duration = 0;
-var add_range = 0;
+// Var values for modifiers are temporary until their code is added.
+int level = 1;
+int mult_duration = 1;
+int mult_range = 1;
+int add_duration = 0;
+int add_range = 0;
 int duration = mult_duration * (5 + level) + add_duration;
 int radius = mult_range * (300 + (30 * level)) + add_range;
