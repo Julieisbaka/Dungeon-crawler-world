38e8b7a57aa7cb6aee8a589aec03653aea54ff55
Julie - Mathman <casperschorr06@gmail.com>
2025-09-01 15:52:47 -0400

    feat: add restroom_distance function to generate random restroom distances

---
+++ src/grid.rs
@@ -0,0 +1,10 @@
+use rand::Rng;
+
+const neighborhood_size: u16 = 1609;
+
+/// Restroom distance is approximately 400m (300-500m)
+/// Returns a random value between 300 and 500 (inclusive).
+pub fn restroom_distance() -> u16 {
+    let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
+    (&mut rng).gen_range(300..=500)
+}
